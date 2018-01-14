822e6f595a563755df58d820a1a8d486965d9642
Maksim <35308641+MaksimBars@users.noreply.github.com>
2018-01-15 00:27:35 +0300

    added loop

--- deck.py
+++ deck.py
@@ -5,7 +5,7 @@ deck = ["Clubs Ace", "Clubs 2", "Clubs 3", "Clubs 4", "Clubs 5", "Clubs 6", "Clu
        "Diamonds Ace", "Diamonds 2", "Diamonds 3", "Diamonds 4", "Diamonds 5", "Diamonds 6", "Diamonds 7", "Diamonds 8", "Diamonds 9", "Diamonds 10", "Diamonds Jack", "Diamonds Quenn", "Diamonds King",
        "Hearts Ace", "Hearts 2", "Hearts 3", "Hearts 4", "Hearts 5", "Hearts 6", "Hearts 7", "Hearts 8", "Hearts 9", "Hearts 10", "Hearts Jack", "Hearts Quenn", "Hearts King",
        "Spades Ace", "Spades 2", "Spades 3", "Spades 4", "Spades 5", "Spades 6", "Spades 7", "Spades 8", "Spades 9", "Spades 10", "Spades Jack", "Spades Quenn", "Spades King"]
-print(len(deck))
+print("Всего карт в колоде: ",len(deck))
 #Перемешиваем колоду.
 random.shuffle(deck)
 
@@ -26,21 +26,30 @@ print("Козырь обьявлен: ",random_deck)
 
 #Раздаем карты.
 hand = deck[0:6]
-print("Ваши карты:",hand)
-#for elem in hand:
-    #print(elem, end=' . ')
+#print("Ваши карты:",hand)
+for elem in hand:
+    print(elem, end=' . ')
 deck = deck[6:]
-print(len(deck))
-#Запрос на номера карт для сброса, значение хранит переменная x.
-s = list(input("\n\nВыберите номера 3 карт для сброса: ").split(','))
-#s = [int(x)-1 for x in s if x.isdigit()]
-for x in s:
+print("\nОсталось карт: ", len(deck))
+#Цикл 
+while deck != 0:
+    #Запрос на сброс карт
+    s = list(input("\n\nВыберите номера 3 карт для сброса: ").split(','))
+#Проверка на ввод цифр
     s1 = []
     for x in s:
         if x.isdigit():
             s1.append(int(x)-1)
-            
-
-
-
-print(s1)
+#Сортировка и удаление карт
+    for n in sorted(s1, reverse=True):
+        hand.pop(n)
+#Подсчет  количества оставшихся карт
+    count_card = len(s)
+#Пополнение руки новыми картами
+    hand += deck[:count_card]
+#Уменьшение колоды по количеству отданых карт в руку
+    deck = deck[count_card:]
+#Переделываем список в строку
+    for elem in hand:
+        print(elem, end=' . ')
+    print("\nОсталось карт: ", len(deck))
