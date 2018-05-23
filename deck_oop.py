e7528ff2f15f4dbe1ac5e29fc8a85ef217bb038f
maksim <4403604@gmail.com>
2018-05-23 22:52:45 +0300

    change the code

--- deck_oop.py
+++ deck_oop.py
@@ -1,27 +1,43 @@
 import random
 
 
-class Card:
-    rank = ['6', '7', '8', '9', '10', 'J', 'Q', 'K', "A"]
-    suits = ['♠', '♦', '♥', '♣']
-    weight = {"6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 11, "Q": 12, "K": 13, "A": 14}
+class Card(object):
+    def __init__(self, suit, rank, weight):
+        self.suit = suit
+        self.rank = rank
+        self.weight = weight
+
+    def __repr__(self):
+        s = '{}{}:{}'.format(self.rank, self.suit, self.weight)
+        return s
+
+class Deck(object):
+    def __init__(self):
+        self.RANK = ['6', '7', '8', '9', '10', 'J', 'Q', 'K', "A"]
+        self.SUIT = ['♠', '♦', '♥', '♣']
+        self.WEIGHT = {"6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 11, "Q": 12, "K": 13, "A": 14}
+        self.deck = []
+        for suit in self.SUIT:
+            for rank in self.RANK:
+                self.deck.append(Card(rank, suit, self.WEIGHT[rank]))
+        random.shuffle(self.deck)
+
+    def tramp_card(self):
+        trump_card = self.deck[-1]
+        return trump_card
+
+    def __len__(self):
+        return len(self.deck)
+
+    def take_card(self):
+        card = self.deck[:6]
+        return card
 
 
-class Deck(Card):
-    stack = [r + s for r in Card.rank for s in Card.suits]  # create a deck of rank and suits
-    random.shuffle(stack)
 
 
-print(Deck.stack)
-print(len(Deck.stack))
-
-
-class Hand(Deck):
-    min_size_hand = 0
-    max_size_hand = 6
-    hand = Deck.stack[0:6]  # cut deck for first distribution
-    Deck.stack = Deck.stack[6:]
-
-
-print(Hand.hand)
-print(Deck.stack)
+d = Deck()
+print(d.deck)
+print(d.tramp_card())
+print(d.__len__())
+print(d.take_card())
