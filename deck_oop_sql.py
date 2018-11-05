e1a9cf65fa4d587e50c5d9fbc0719e87b212ebe6
MaksimBars <4403604@gmail.com>
2018-11-05 16:27:41 +0300

    Changed the code to add to the database.

--- deck_oop_sql.py
+++ deck_oop_sql.py
@@ -2,39 +2,37 @@ import sqlite3
 import datetime
 
 
-# тестовый заполнитель
-def aggregate():
-    time = "{:%B %d, %Y}".format(datetime.datetime.now())
-    x = [x for x in range(5)]
-    tes_date = []
-    for i in x:
-        if i % 2 == 0:
-            tes_date.append(('True', time))
-        else:
-            tes_date.append(('False', time))
-    return tes_date
+def log_winner(who_winner):
+    date = "{:%B %d, %Y}".format(datetime.datetime.now())
+    conn = sqlite3.connect('deck.db')
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO tournament (is_player_win, date) VALUES (?, ?)', (who_winner, date))
+    conn.commit()
+    conn.close()
 
-
-conn = sqlite3.connect('deck.db')
-cursor = conn.cursor()
-# sql_crate = """CREATE TABLE tournament ('is_player_win','date')"""
+# sql_crate = """CREATE TABLE tournament ('is_player_win','date','remained card')"""
 # cursor.execute(sql_crate)  # создание БД с указанными таблицами
-# commit
+# conn.commit()
+# conn.close()
 # ----------------------------------------------------------------------------
 # поплонение данными
-# for i in db_test_code.s:
+# for i in db_test_code:
 #     cursor.execute('INSERT INTO tournament (is_player_win, date) VALUES (?, ?)', (i))
 #     conn.commit()
+#     conn.close()
+
 # ----------------------------------------------------------------------------
 # удаление из таблицы
 # del_db = """DELETE FROM tournament WHERE is_player_win ==1 and date == 123"""
 # cursor.execute(del_db)
-# commit
+# conn.commit()
+# conn.close()
 # ----------------------------------------------------------------------------
 # обновление данных
 # update_db = """UPDATE tournament set date = 'October 31, 2018' WHERE OID = 4"""
 # cursor.execute(update_db)
-# commit
+# conn.commit()
+# conn.close()
 # ----------------------------------------------------------------------------
 # выборка строк из столбца и вывод в консоль
 # x = cursor.execute("""SELECT * FROM tournament WHERE is_player_win == 'True'""")
